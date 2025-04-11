5becca93458cdf27071c52603b4959178627ae8b
Hadj Taieb Imen <115839915+Imen-Ht07@users.noreply.github.com>
2025-04-11 14:46:23 +0200

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AddEditFournisseurComponent } from './components/Fournisseur/add-edit-fournisseur/add-edit-fournisseur.component';
+import { ConsultFournisseurComponent } from './components/Fournisseur/consult-fournisseur/consult-fournisseur.component';
+
 
 const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'add-fournisseur', component: AddEditFournisseurComponent },
+  { path: 'updt-fournisseur/:id', component: AddEditFournisseurComponent },
+  { path: 'consult-fournisseur', component: ConsultFournisseurComponent },
   
 ];
 
